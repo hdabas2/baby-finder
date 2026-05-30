617901611ce3c9d1549b186eb8de3b7782994b4b
hdabas2 <67456583+hdabas2@users.noreply.github.com>
2026-05-30 23:21:16 +0530

    Update find_baby_videos.py

--- find_baby_videos.py
+++ find_baby_videos.py
@@ -9,7 +9,7 @@ from insightface.app import FaceAnalysis
 # =====================================
 
 REFERENCE_FOLDER = r"D:\baby-finder\reference"
-SEARCH_FOLDER = r"E:\Harsh_phone\backup-May2026"
+SEARCH_FOLDER = r"E:\mobile\data"
 MATCH_FOLDER = r"D:\baby-finder\matches\videos"
 
 SIMILARITY_THRESHOLD = 0.45
